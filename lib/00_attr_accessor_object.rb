1998c6c091e7baef8e652907f57eda3189755bec
adavies <arielpdavies@gmail.com>
2021-05-25 16:44:54 -0400

    Section 00 finished

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,23 @@
 class AttrAccessorObject
-  def self.my_attr_accessor(*names)
-    # ...
+  def self.my_attr_accessor(*names) # [:age,:username,:password]
+      names.each do |name|
+        
+      define_method(name) do 
+        # "@#{name}"  "@age" => @age
+        instance_variable_get("@#{name}")
+      end
+
+      define_method("#{name}=") do |value|
+        instance_variable_set("@#{name}", value)
+      end
+    end
   end
+
+#   def age
+#     @age
+#   end
+
+#   def age=(num)
+#     @age = num
+#   end
 end
